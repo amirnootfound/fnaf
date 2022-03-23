f2c9704f572c7ae3af77e62e762255addaa5b546
AmirChek7 <amirbekmamasaliev3@gmail.com>
2022-03-23 20:10:41 +0600

    doesnt completed v1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+}) 
+
+console.log(modalBtn);
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
